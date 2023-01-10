0f71b0a95d9c25f4031a20b6aa95e9b5f6666980
Debadree Chatterjee <debadree333@gmail.com>
2023-01-11 01:56:56 +0530

    fixup! move to promise based approach

--- test/parallel/test-webstreams-finished.js
+++ test/parallel/test-webstreams-finished.js
@@ -9,9 +9,32 @@ const { finished } = require('stream');
   const rs = new ReadableStream({
     start(controller) {
       controller.enqueue('asd');
+      controller.close();
     },
   });
-  finished(rs, common.mustCall(() => {
-    
+  finished(rs, common.mustSucceed());
+  async function test() {
+    const values = [];
+    for await (const chunk of rs) {
+      values.push(chunk);
+    }
+    assert.deepStrictEqual(values, ['asd']);
+  }
+  test();
+}
+
+{
+  let str = '';
+  const ws = new WritableStream({
+    write(chunk) {
+      console.log(chunk);
+      str += chunk;
+    }
+  });
+  finished(ws, common.mustSucceed(() => {
+    assert.strictEqual(str, 'asd');
   }));
+  const writer = ws.getWriter();
+  writer.write('asd');
+  writer.close();
 }
